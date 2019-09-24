63bae3057114f3dfa2ea829e522fc5af18efd6f6
John Alamina <john.alamina@netsells.co.uk>
2019-09-24 05:56:46 +0100

    default-run cargo

--- rmenu/src/edit.rs
+++ rmenu/src/edit.rs
@@ -1,3 +1,4 @@
+use cursive::traits::*;
 use cursive::views::{Dialog, EditView, TextView};
 use cursive::Cursive;
 
@@ -5,7 +6,7 @@ fn main() {
     let mut siv = Cursive::default();
 
     // Create a dialog with an edit text and a button.
-    // The user can either hit the &lt;Ok&gt; button,
+    // The user can either hit the <Ok> button,
     // or press Enter on the edit text.
     siv.add_layer(
         Dialog::new()
@@ -27,14 +28,14 @@ fn main() {
                 // This will run the given closure, *ONLY* if a view with the
                 // correct type and the given ID is found.
                 let name = s
-                    .call_on_id("name", |view: &amp;mut EditView| {
+                    .call_on_id("name", |view: &mut EditView| {
                         // We can return content from the closure!
                         view.get_content()
                     })
                     .unwrap();
 
                 // Run the next step
-                show_popup(s, &amp;name);
+                show_popup(s, &name);
             }),
     );
 
@@ -43,7 +44,7 @@ fn main() {
 
 // This will replace the current layer with a new popup.
 // If the name is empty, we'll show an error message instead.
-fn show_popup(s: &amp;mut Cursive, name: &amp;str) {
+fn show_popup(s: &mut Cursive, name: &str) {
     if name.is_empty() {
         // Try again as many times as we need!
         s.add_layer(Dialog::info("Please enter a name!"));
