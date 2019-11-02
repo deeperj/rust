91e7bd02e2c4bae89b06bfced08d7ebca4414844
John Alamina <john.alamina@gmail.com>
2019-11-02 10:01:47 +0000

    tutorial output

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+mod datatypes;
+mod output;
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+  let command = args[1].clone();
+
+  // println!("Command: {}", command);
+
+  if command == "datatypes" {
+    datatypes::run();
+  } else if command == "status" {
+    output::run();
+  } else {
+    println!("That is not a valid command");
+  }
 }
