0399b09c50df14afe6a2e6f2d4cf2d65c50f952e
John Alamina <john.alamina@netsells.co.uk>
2019-11-03 04:41:10 +0000

    embedded00

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 mod datatypes;
 mod output;
+mod math;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -12,6 +13,8 @@ fn main() {
     datatypes::run();
   } else if command == "output" {
     output::run();
+  } else if command == "math" {
+    math::run();
   } else {
     println!("That is not a valid command");
   }
