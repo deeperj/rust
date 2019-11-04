df217743ee100ec679a30cbef648ff481ed5db25
John Alamina <john.alamina@gmail.com>
2019-11-04 21:14:05 +0000

    drag00

--- dart/web/jadeabc/lib/main.dart
+++ dart/web/jadeabc/lib/main.dart
@@ -77,7 +77,10 @@ class _MyHomePageState extends State<MyHomePage>
       // print("value has been changed to $_stop_char");
     });
   }
-void _incrementCounter() {
+  void _handle(LongPressEndDetails details) {
+    _incrementCounter();
+  }
+  void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -142,81 +145,82 @@ void _incrementCounter() {
         .height
         .toInt() - 80;
     return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: Text(widget.title),
-          ),
-          body: Stack(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            children: <Widget>[
-          Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          // mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Slider(
-                value:_stop_char.toDouble(),
-                onChanged: _setup,
-                max: 26,
-                min: 1,
-                divisions: 25,
-              ),
-              Text(
-                'Stops at ${_abc[_stop_char]}',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .display1,
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-          new Positioned(
-            left: _animation.value.dx,
-            top: _animation.value.dy,
-            child: GestureDetector(
-              child: Container(
-                width: _width.toDouble(),
-                height: _height.toDouble(),
-                decoration: new BoxDecoration(color: Colors.red),
-                child: Text(
-                  '${_abc[_counter]}',
+      child: Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          children: <Widget>[
+        Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Invoke "debug painting" (press "p" in the console, choose the
+        // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        // to see the wireframe for each widget.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Slider(
+              value:_stop_char.toDouble(),
+              onChanged: _setup,
+              max: 26,
+              min: 1,
+              divisions: 25,
+            ),
+            Text(
+              'Stops at ${_abc[_stop_char]}',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .display1,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+        new Positioned(
+          left: _animation.value.dx,
+          top: _animation.value.dy,
+          child: GestureDetector(
+            child: Container(
+              width: _width.toDouble(),
+              height: _height.toDouble(),
+              decoration: new BoxDecoration(color: Colors.red),
+              child: Text(
+                '${_abc[_counter]}',
 //                  style: Theme
 //                      .of(context)
 //                      .textTheme
 //                      .display1,
-                  style: TextStyle(fontSize: 120),
-                  textAlign: TextAlign.center,
-                ),
+                style: TextStyle(fontSize: 120),
+                textAlign: TextAlign.center,
               ),
-              onTap: _incrementCounter,
-              onLongPress: _incrementCounter,
-              //onLongPressEnd: _incrementCounter,
             ),
-          )],
+            onTap: _incrementCounter,
+            //onLongPress: _incrementCounter,
+            onLongPressEnd: _handle,
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ), // This trailing comma makes auto-formatting nicer for build methods.
-        ));
+        )],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+    );
   }
 }
