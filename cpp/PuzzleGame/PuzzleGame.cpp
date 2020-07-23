bbcdbbb118b9aef1900f2cf8f49ce1941c61e38c
Iyalla John Alamina <I.Alamina2@hud.ac.uk>
2020-07-23 22:09:00 +0100

    pz20

---
+++ cpp/PuzzleGame/PuzzleGame.cpp
@@ -0,0 +1,47 @@
+#include "PuzzleGame.hpp"
+
+PuzzleGame::PuzzleGame(){}
+void PuzzleGame::checkChoice(){}
+bool PuzzleGame::checkWin(){
+    return false;//dummy implementation
+}
+void PuzzleGame::swapPiece(){}
+void PuzzleGame::drawBoard(){}
+void PuzzleGame::randomizeBoard(){}
+/*
+private
+    2. void checkChoice();
+    4. void swapPiece();
+    5. void drawBoard();
+    6. void randomizeBoard();
+    3. bool checkWin();
+    void getChoice();
+public:
+    int board[];
+    1. PuzzleGame();
+    void startGame();
+
+
+
+  int board[BSZ2]={1,4,2,6,0,5,7,3,8};//flattened 1D
+  while(!winner){ //game loop or game logic
+  //disply the board using a nested for loop
+    //get input
+     //first display enter number
+     // declare var for input
+     //use standard input cin
+   //swap those values
+    //*search for the value v
+    // declare the index of v as idv
+    //search for the zero value
+    //declare id0 as the index of 0
+   //check for winner
+    //check for winner step 1 check first and last board space
+      continue;//continue containing loop
+    //check for winner step 2 check remaining board spaces
+    // done checking for winner
+   //printboard one last time
+  }//game loop ends here
+  //out of the while loop
+
+*/
