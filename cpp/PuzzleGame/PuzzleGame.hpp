5f76371db54ac8c34f4ab5887bb52270509a42bd
Iyalla John Alamina <I.Alamina2@hud.ac.uk>
2020-07-31 15:29:28 +0100

    pz23

--- cpp/PuzzleGame/PuzzleGame.hpp
+++ cpp/PuzzleGame/PuzzleGame.hpp
@@ -5,16 +5,25 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+const int BSZ = 3;
+const int BSZ2 = BSZ * BSZ;
+
 class PuzzleGame {
 private:
-    void checkChoice();
+    bool winner;
+    int choice,id0,idc;
+    bool choiceValid();
     void swapPiece();
     void drawBoard();
     void randomizeBoard();
     bool checkWin();
-    void getChoice();
+    int getChoice();
+    int find(int);
 public:
-    int board[];
+    friend class Test;
+    int board[BSZ2] = {1,4,2,
+                       6,0,5,
+                       7,3,8};
     PuzzleGame();
     void startGame();
 };
