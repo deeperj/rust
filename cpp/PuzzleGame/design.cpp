78d5b8ccf8edb1db02b794fa683dd4442c3fde7e
John Alamina <john.alamina@gmail.com>
2020-08-02 05:00:31 +0100

    pz27

--- cpp/PuzzleGame/design.cpp
+++ cpp/PuzzleGame/design.cpp
@@ -3,12 +3,16 @@
 
 int main()
 {
-	cout << "PuzzleGame files added!" << endl;
-	cout << "Puzzle dummy implementation in progress!" << endl;
-	initscr();
-	Test t;
-	t.testall();
-	endwin();
-	return 0;
+  cout << "PuzzleGame files added!" << endl;
+  cout << "Puzzle dummy implementation in progress!" << endl;
+  initscr();
+  WINDOW* obox = newwin(3, 10, 9, 14);
+  refresh();
+  box(obox, 0, 0);
+  mvwprintw(obox, 1, 1, "   Ok");
+  wrefresh(obox);
+  getch();
+  endwin();
+  Test t;
+  t.testall();
 }
-
