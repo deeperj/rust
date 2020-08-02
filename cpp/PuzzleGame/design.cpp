58328ecfaa5533c9c6d268b474aba3816772c2fc
John Alamina <john.alamina@gmail.com>
2020-08-02 04:36:50 +0100

    pz26

--- cpp/PuzzleGame/design.cpp
+++ cpp/PuzzleGame/design.cpp
@@ -5,8 +5,10 @@ int main()
 {
 	cout << "PuzzleGame files added!" << endl;
 	cout << "Puzzle dummy implementation in progress!" << endl;
+	initscr();
 	Test t;
 	t.testall();
+	endwin();
 	return 0;
 }
 
