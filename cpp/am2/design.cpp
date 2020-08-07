aedf1b6bb709d75fe54cfaff58abc30e0020bd07
John Alamina <john.alamina@gmail.com>
2020-08-07 06:55:44 +0100

    am00

---
+++ cpp/am2/design.cpp
@@ -0,0 +1,12 @@
+#include "managerUI.h"
+int main() {
+	//int main1(int argc, char **argv) {
+	// initialise
+	// setup memory and clear screen
+	Amtui ui;
+	//ui.drawBorder();
+	ui.run();
+	getch();
+	return 0;
+}
+
