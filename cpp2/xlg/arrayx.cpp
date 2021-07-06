fd1c9b7871993cce8f098003f6f128955f742c4e
john Alamina <john.alamina@hud.ac.uk>
2021-07-06 12:57:40 +0100

    arrayx

---
+++ cpp2/xlg/arrayx.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    const int size = 3;
+    int array[size];//3 is the size, the last address is size - 1 = 3-1=2;
+
+    
+}
